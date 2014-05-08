a41bc1aae6e7519287d4cdd2094978f709ca03a2
satchelspencer <satchelspencer@gmail.com>
2014-05-08 15:30:27 -0600

    5/8/14_3
    I golf’d your mother so hard hahahaha

--- a/edit.js
+++ a/edit.js
@@ -17,27 +17,14 @@ function selectEl(id){
 		$("positioncanvas").show();
 		$("positionsequence").hide();
 		var p = $(id).j.position;
-		$("positionnw").style.background = (p.top && p.left) ? "#575757" : "#474747";
-		$("positionn").style.background = (p.top && p.centerx) ? "#575757" : "#474747";
-		$("positionne").style.background = (p.top && p.right) ? "#575757" : "#474747";
-		$("positionw").style.background = (p.left && p.centery) ? "#575757" : "#474747";
-		$("positionc").style.background = (p.centerx && p.centery) ? "#575757" : "#474747";
-		$("positione").style.background = (p.right && p.centery) ? "#575757" : "#474747";
-		$("positionsw").style.background = (p.bottom && p.left) ? "#575757" : "#474747";
-		$("positions").style.background = (p.bottom && p.centerx) ? "#575757" : "#474747";
-		$("positionse").style.background = (p.bottom && p.right) ? "#575757" : "#474747";
-		$("positiontop").style.display = (p.top) ? "block" : "none";
-		$("originnw").style.background = (p.top) ? isAbsolute(p.top) ? "white" : "red" : "white";
-		$("toplabel").innerHTML = p.top+(isAbsolute(p.top) ? "px" : "");
-		$("positionleft").style.display = p.left ? "block" : "none";
-		$("originsw").style.background = p.left ? isAbsolute(p.left) ? "white" : "red" : "white";
-		$("leftlabel").innerHTML = p.left+(isAbsolute(p.left) ? "px" : "");
-		$("positionbottom").style.display = p.bottom ? "block" : "none";
-		$("originse").style.background = p.bottom ? isAbsolute(p.bottom) ? "white" : "red" : "white";
-		$("bottomlabel").innerHTML = p.bottom+(isAbsolute(p.bottom) ? "px" : "");
-		$("positionright").style.display = p.right ? "block" : "none";
-		$("originne").style.background = p.right ? isAbsolute(p.right) ? "white" : "red" : "white";
-		$("rightlabel").innerHTML = p.right+(isAbsolute(p.right) ? "px" : "");
+		var i = {"nw":["top", "left"],"n":["top", "centerx"],"ne":["top", "right"],"w":["left","centery"],"c":["centerx", "centery"],"e":["right", "centery"],"sw":["bottom","left"],"s":["bottom","centerx"],"se":["bottom","right"]};
+		for(x in i) $("position"+x).style.background = p[i[x][0]] && p[i[x][1]] ? "#575757" : "#474747";
+		var o = {top : "nw", left : "sw", bottom : "se", right : "ne"};
+		for(x in o){
+			$("position"+x).style.display = (p[x]) ? "block" : "none";
+			$("origin"+o[x]).style.background = (p[x]) ? isAbsolute(p[x]) ? "white" : "red" : "white";
+			$(x+"label").innerHTML = p[x]+(isAbsolute(p[x]) ? "px" : "");
+		}
 	}else if(pType == "sequence"){
 		$("positionsequence").show();
 		$("positioncanvas").hide();
