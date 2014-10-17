faaccb2ff1bea9959f2cfc7e5ac4bfe9a6b78c69
satchelspencer <satchelspencer@gmail.com>
2014-10-16 19:20:11 -0600

    10/16/14 (setCss for bg, font and border)

--- js/render.js
+++ js/render.js
@@ -29,17 +29,30 @@ function newEl(data, parentType, addr){
 		if(data.color) this.css("backgroundColor", "rgba("+data.color+")");
 		if(data.image){
 			this.css("backgroundImage", "url('../as"+data.image+"')");
-			if(data.repeat){
-				
-			}
+			if(data.repeat) this.css("backgroundRepeat", data.repeat);
+			if(data.size) this.css("backgroundSize", data.size);
 		}
 	};
 	el.font = function(data){
-	
+		if(data.family) this.css("fontFamily", data.family);
+		if(data.size) this.css("fontSize", data.family);
+		if(data.color) this.css("fontSize", "rgba("+data.color+")");
+		if(data.align) this.css("textAlign", data.align);
+		if(data.bold) this.css("fontWeight", data.bold=="1"?"bold":"normal");
+		if(data.underline) this.css("textDecoration", data.underline=="1"?"underline":"none");
+		if(data.italic) this.css("fontStyle", data.italic=="1"?"italic":"normal");
 	};
 	el.border = function(data){
-	
+		var borderStyle = "";
+		if(data.width) this.css("borderWidth", data.width+"px");
+		var dirs = ["Top", "Left", "Bottom", "Right"];
+		if(data.color) this.css("borderColor", "rgba("+data.color+")");
+		if(data.radius) this.css("borderRadius", data.radius+"px");
+		if(data.style && data.edges){
+			for(var e in data.edges) this.css("border"+dirs[e]+"Style", data.edges[e] == "1"?data.style:"none");
+		}
 	};
+	el.event("click", log);
 	return el;
 }
 function jsonByAddr(addr){
